f2dc65e70d856aba0d9125ffbb9e38eba83ae80d
urii urii2 <urioleh@gmail.com>
2022-09-09 17:28:55 +0200

    Finish day page

---
+++ weather-app/src/components/DayCard.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { v4 as uuidv4 } from "uuid";
+import { calcRainChanceIcon } from "../hooks/calcRainChanceIcon";
+import styles from './DayCard.module.scss'
+
+function DayCard({ day }) {
+
+    let count = 0
+    
+
+    return (
+
+            <div className={styles.containerDay}>
+                {day.hourly_rain_chance.map((chance, i) => {
+                    const icon = calcRainChanceIcon(chance);
+                    count += 1
+
+                    const temp = day.hourly_temperature.map((temp, j) => {
+                        if (i === j) {
+                            return <span style={temp > 20 ? {color:'var(--red)'}: {color:'var(--blue)'}}>{temp}</span>
+                        }
+                    })
+                    return (
+                        <div className={styles.hour} key={uuidv4()}>
+                            <h4>{count}h</h4>
+                            <img src={`assets/svg-weather/${icon}.svg`} alt="" />
+                            <div className={styles.chance}>
+                                <p>{chance}%</p>
+                                <p className={styles.chanceText}>rain chance</p>
+                            </div>
+                            <p ><strong>{temp}</strong>ºC</p>
+                        </div>
+                    );
+                })}
+            </div>
+
+    );
+}
+
+export default DayCard;
--- weather-app/src/pages/Day.jsx
+++ weather-app/src/pages/Day.jsx
@@ -1,40 +1,47 @@
-import axios from 'axios'
-import moment from 'moment'
-import React, { useCallback, useEffect, useState } from 'react'
+import axios from "axios";
+import moment from "moment";
+import React, { useCallback, useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
-import { calcAverage } from '../hooks/calcAverage'
-import { calcRainChanceIcon } from '../hooks/calcRainChanceIcon';
+import DayCard from "../components/DayCard";
+import { calcAverage } from "../hooks/calcAverage";
 
-function Day({ children, drop }) {
+function Day({ children, drop, city, country }) {
     const [day, setDay] = useState("");
-    const [icon, setIcon] = useState('');
     const [averageRain, setAverageRain] = useState("");
     const [searchParams] = useSearchParams();
+    const [dayName, setDayName] = useState("");
 
     const getDay = useCallback(async () => {
+        const date = searchParams.get("date");
+        setDayName(moment(date).format("dddd"));
 
-        const date = searchParams.get('date')
-  
-          const response = await axios.get(`http://localhost:5000/weather-api-7c25c/us-central1/app/api/weather/find?date=${date}&location=${drop}`)
-          const day = response.data.resultado[0].doc
-          calcAverage(day.hourly_rain_chance, setAverageRain)
-          setDay(day)
-          setIcon(calcRainChanceIcon(averageRain))
-          console.log(day)
-      }, [averageRain, drop, icon])
+        const response = await axios.get(
+            `http://localhost:5000/weather-api-7c25c/us-central1/app/api/weather/find?date=${date}&location=${drop}`
+        );
+        const day = response.data.resultado[0].doc;
+        calcAverage(day.hourly_rain_chance, setAverageRain);
+        setDay(day);
+        console.log(day);
+    }, [averageRain, drop]);
 
-      useEffect(() => {
-        getDay()
-        console.log(searchParams.get('date'))
-      }, [getDay]);
+    useEffect(() => {
+        getDay();
+        console.log(searchParams.get("date"));
+    }, [getDay]);
 
-  return (
-    <div>
-        {children}
-        {day.date}
-        <img  src={`assets/svg-weather/${icon}.svg`} alt="" />
-    </div>
-  )
+    return (
+        <div className="day">
+            {children}
+            <div className="containerTitles">
+                <h4 className="dayName">{dayName}</h4>
+                <h2 className="city">{city}</h2>
+                <h3 className="country">{country}</h3>
+            </div>
+            {day && (
+                <DayCard day={day}/>
+            )}
+        </div>
+    );
 }
 
-export default Day
+export default Day;
